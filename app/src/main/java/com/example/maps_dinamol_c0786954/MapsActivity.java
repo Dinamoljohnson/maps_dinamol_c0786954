8c6731c81b5943b0e6618ea7a5e25b8f9a0185d8
Dinamoljohnson <dinamol4141@gmail.com>
2021-06-10 19:07:05 +0530

    quadrilateral Added

--- app/src/main/java/com/example/maps_dinamol_c0786954/MapsActivity.java
+++ app/src/main/java/com/example/maps_dinamol_c0786954/MapsActivity.java
@@ -178,7 +178,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 for (int i=0; i<QUADRILATERAL_SIDES; i++) {
                     options.add(markers.get(i).getPosition());
                 }
-
                 shape = mMap.addPolygon(options);
 
             }
